73fe82998de720643f840a5a1f4900d57b54b3ec
Johan Walter <walter.johan25@gmail.com>
2022-08-25 15:16:14 +0200

    création fenêtre modal + formulaire HTML/CSS/JS done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// Création du lien entre le bouton contact et son contenu
+const modalContainer = document. querySelector(".modal-container");// selectionne le premier élément avec pour classe .modal-container
+const modalTriggers = document. querySelectorAll(".modal-trigger"); // selectionne tous les éléments .modal-trigger
+
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
+// pour chaque evenement trigger,
+
+function toggleModal() {
+  modalContainer.classList.toggle("active")
+
+}
