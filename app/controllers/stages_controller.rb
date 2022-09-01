9d823939b6c45b953b5cab4936fa6ff1da65203d
Johan Walter <walter.johan25@gmail.com>
2022-09-01 16:55:07 +0200

    création page add/delete pour ajout et suppression de stage

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -2,4 +2,35 @@ class StagesController < ApplicationController
   def index
     @stages = Stage.all
   end
+
+  def new
+    @stage = Stage.new
+  end
+
+  def create
+    @stage = Stage.new(stage_params)
+    @stage.save
+
+  redirect_to stages_path(@stages)
+
+  end
+
+  def show
+    @stage = Stage.find(params[:id])
+  end
+
+  def destroy
+    @stage = Stage.find(params[:id])
+    @stage.destroy
+
+    redirect_to stages_path(@stages)
+  end
+
+
+  private
+
+  def stage_params
+    params.require(:stage).permit(:title, :date, :participants, :price, :place, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,16 @@ Rails.application.routes.draw do
 
   get "disciplines", to: "pages#disciplines"
 
-  get "news", to: "pages#news"
+  # get "news", to: "pages#news" // pour que le site devienne dynamique on a modifié la fonction et le chemin de stages vers index
 
   get "stages", to: "stages#index"
+
+  get "stages/new", to: "stages#new"
+
+  post "stages", to: "stages#create"
+
+  get "stages/:id", to: "stages#show", as: :stage
+
+  delete "stages/:id", to: "stages#destroy"
 
 end
